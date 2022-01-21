4cc402e6857e4d15d0648f8fd576e51f303d7b53
Aqeelhassan23695845 <97553811+Aqeelhassan23695845@users.noreply.github.com>
2022-01-22 06:10:13 +1030

    Basic Calculation and remaining GUI
    i have done basic calculation addition subtraction multiplication and division done some work on brackets as well .
    GUI buttons are almost done and working except few.  and off button is able to dispose the calculator.

--- Prog_5001_Task_1/Calculator.java
+++ Prog_5001_Task_1/Calculator.java
@@ -3,6 +3,10 @@
 import javax.swing.*;  
 import java.awt.*;  
 import java.awt.event.*;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Write a description of class Calculator here.
@@ -39,7 +43,7 @@ public class Calculator extends JFrame implements ActionListener
   JButton off=new JButton("OFF");
   JButton backspace=new JButton(">>");
   JButton point=new JButton(".");
-  
+  Label headerLabel =new Label("My PROG5001 - Calculator(Version)");
 
 /**
  * Constructor for objects of class Calculator using to Call calcGUI method and set title as well as size
@@ -48,7 +52,9 @@ public Calculator()
 {
     //Calling calcGUI method, set calculator title and size 
     super("Calculator");
-    setSize(300, 350);
+    setSize(350, 420);
+    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
     calcGUI();
 
 }
@@ -57,24 +63,35 @@ public Calculator()
 {
     // Creating Top panel for calculator text area
     JPanel topPanel = new JPanel();
+    topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     GridLayout topPanelLayout = new GridLayout(0,1);
+    topPanelLayout.setVgap(10);
     topPanel.setLayout(topPanelLayout);
 
     // creating centerpanellayout for calculator buttons from 0 to 9 and equals to =
-    JPanel centerPanel = new JPanel();       
+    JPanel centerPanel = new JPanel();  
+    centerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     GridLayout centerPanelLayout = new GridLayout(5,3,0,0);
+    centerPanelLayout.setHgap(10);
+    centerPanelLayout.setVgap(10);
     centerPanel.setLayout(centerPanelLayout);
 
     // creating rightpanel for operators and other functionalties button
     JPanel rightPanel = new JPanel();
+    rightPanel.setBorder(BorderFactory.createEmptyBorder(10, 0,10, 10));
     GridLayout rightPanelLayout = new GridLayout(5,2,8,5);
+    rightPanelLayout.setHgap(10);
+    rightPanelLayout.setVgap(10);
     rightPanel.setLayout(rightPanelLayout);
+    
+    // adding header label
+    headerLabel.setBackground(Color.decode("#a5c3e6"));
+    topPanel.add(headerLabel);
 
     //Adjusting text field font, alignment, size and value        
     calText.setFont(new Font("Arial", Font.BOLD, 24));
     calText.setHorizontalAlignment(JTextField.RIGHT);        
     calText.setPreferredSize(new Dimension(50,45));
-    calText.setText("0");
     topPanel.add(calText);
     
     // adding panel to jframe
@@ -85,37 +102,37 @@ public Calculator()
     // creating button 1 with action perform listner as well as color and adding to panel
     one.setActionCommand("1");
     one.addActionListener(this);
-    one.setBackground(Color.decode("#c9c9c9"));
+    one.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(one);
     
     // creating button 2 with action perform listner as well as color and adding to panel
     two.setActionCommand("2");
     two.addActionListener(this);
-    two.setBackground(Color.decode("#c9c9c9"));
+    two.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(two);
     
     // creating button 3 with action perform listner as well as color and adding to panel
     three.setActionCommand("3");
     three.addActionListener(this);
-    three.setBackground(Color.decode("#c9c9c9"));
+    three.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(three);
     
     // creating button 4 with action perform listner as well as color and adding to panel
     four.setActionCommand("4");
     four.addActionListener(this);
-    four.setBackground(Color.decode("#c9c9c9"));
+    four.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(four);
     
     // creating button 5 with action perform listner as well as color and adding to panel
     five.setActionCommand("5");
-    five.setBackground(Color.decode("#c9c9c9"));
+    five.setBackground(Color.decode("#cbcbcb"));
     five.addActionListener(this);
     centerPanel.add(five);
     
     // creating button 6 with action perform listner as well as color and adding to panel
     six.setActionCommand("6");
     six.addActionListener(this);
-    six.setBackground(Color.decode("#c9c9c9"));
+    six.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(six);
     
     // creating button 7 with action perform listner as well as color and adding to panel
@@ -127,51 +144,51 @@ public Calculator()
     // creating button 8 with action perform listner as well as color and adding to panel
     eight.setActionCommand("8");
     eight.addActionListener(this);
-    eight.setBackground(Color.decode("#c9c9c9"));
+    eight.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(eight);
     
     // creating button 9 with action perform listner as well as color and adding to panel
     nine.setActionCommand("9");
     nine.addActionListener(this);
-    nine.setBackground(Color.decode("#c9c9c9"));
+    nine.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(nine);
     
     // creating button abs with action perform listner as well as color and adding to panel
     abs.setActionCommand("+/-");
-    abs.setBackground(Color.decode("#c9c9c9"));
+    abs.setBackground(Color.decode("#cbcbcb"));
     abs.addActionListener(this);
     centerPanel.add(abs);
     
     // creating button zero with action perform listner as well as color and adding to panel
     zero.setActionCommand("0");
-    zero.setBackground(Color.decode("#c9c9c9"));
+    zero.setBackground(Color.decode("#cbcbcb"));
     zero.addActionListener(this);
     centerPanel.add(zero);
     
     // creating button point with action perform listner as well as color and adding to panel
     point.setActionCommand(".");
     point.addActionListener(this);
-    point.setBackground(Color.decode("#c9c9c9"));
+    point.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(point);
     
     // creating button = with action perform listner as well as color and adding to panel
     equals1.setActionCommand("=");
     equals1.addActionListener(this);
     equals1.setBorder(BorderFactory.createEmptyBorder());
-    equals1.setBackground(Color.decode("#c9c9c9"));
+    equals1.setBackground(Color.decode("#cbcbcb"));
     centerPanel.add(equals1);
     
     // creating button = with action perform listner as well as color and adding to panel
     equals2.setActionCommand("=");
     equals2.addActionListener(this);
-    equals2.setBackground(Color.decode("#c9c9c9"));
+    equals2.setBackground(Color.decode("#cbcbcb"));
     equals2.setBorder(BorderFactory.createEmptyBorder());
     centerPanel.add(equals2);
     
     // creating button = with action perform listner as well as color and adding to panel
     equals3.setActionCommand("=");
     equals3.addActionListener(this);
-    equals3.setBackground(Color.decode("#c9c9c9"));
+    equals3.setBackground(Color.decode("#cbcbcb"));
     equals3.setBorder(BorderFactory.createEmptyBorder());
     centerPanel.add(equals3);
 
@@ -179,61 +196,61 @@ public Calculator()
     // creating button plus with action perform listner as well as color and adding to panel
     plus.setActionCommand("+");
     plus.addActionListener(this);
-    plus.setBackground(Color.decode("#c9c9c9"));
+    plus.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(plus);
     
     // creating button backspace with action perform listner as well as color and adding to panel
     backspace.setActionCommand(">>");
-    backspace.setBackground(Color.decode("#c9c9c9"));
+    backspace.setBackground(Color.decode("#cbcbcb"));
     backspace.addActionListener(this);
     rightPanel.add(backspace);
     
     // creating button minus with action perform listner as well as color and adding to panel
     minus.setActionCommand("-");
     minus.addActionListener(this);
-    minus.setBackground(Color.decode("#c9c9c9"));
+    minus.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(minus);
     
     // creating button clear with action perform listner as well as color and adding to panel
     clear.setActionCommand("C");
     clear.addActionListener(this);
-    clear.setBackground(Color.decode("#c9c9c9"));
+    clear.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(clear);
     
     // creating button * with action perform listner as well as color and adding to panel
     times.setActionCommand("*");
     times.addActionListener(this);
-    times.setBackground(Color.decode("#c9c9c9"));
+    times.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(times);
     
     // creating button openBracket with action perform listner as well as color and adding to panel
     openBracket.setActionCommand("(");
     openBracket.addActionListener(this);
-    openBracket.setBackground(Color.decode("#c9c9c9"));
+    openBracket.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(openBracket);
 
     // creating button divide with action perform listner as well as color and adding to panel
     divide.setActionCommand("/");
     divide.addActionListener(this);
-    divide.setBackground(Color.decode("#c9c9c9"));
+    divide.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(divide);
     
     // creating button closeBracket with action perform listner as well as color and adding to panel
     closeBracket.setActionCommand(")");
     closeBracket.addActionListener(this);
-    closeBracket.setBackground(Color.decode("#c9c9c9"));
+    closeBracket.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(closeBracket);
     
     // creating button apostrophy with action perform listner as well as color and adding to panel
     apostrophy.setActionCommand("!");
     apostrophy.addActionListener(this);
-    apostrophy.setBackground(Color.decode("#c9c9c9"));
+    apostrophy.setBackground(Color.decode("#cbcbcb"));
     rightPanel.add(apostrophy);
     
     // creating button OFF with action perform listner as well as color and adding to panel
     off.setActionCommand("OFF");
     off.addActionListener(this);
-    off.setBackground(Color.decode("#fdbb2d"));
+    off.setBackground(Color.decode("#f6b79d"));
     rightPanel.add(off);
 }
     
@@ -241,15 +258,13 @@ public void actionPerformed(ActionEvent e)
 {
     // creating variable for operator , result, firstnumber as well as calcText for getting text from textfield
     String calcText;
-    Double firstNumber=0.00;
-    String operator = "";
-    String result = "";
+    String abs="+";
     calcText= calText.getText();
     String command = e.getActionCommand();
     
     // creating conditions for button 1 click
     if (command.equals("1")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
             calcText = "1";
         }else{
         calcText += one.getText();
@@ -258,7 +273,7 @@ public void actionPerformed(ActionEvent e)
     
     // creating conditions for button 2 click
     if (command.equals("2")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "2";
     }else{
         calcText += two.getText();
@@ -267,7 +282,7 @@ public void actionPerformed(ActionEvent e)
     
     // creating conditions for button 3 click
     if (command.equals("3")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "3";
     }
     else{
@@ -277,7 +292,7 @@ public void actionPerformed(ActionEvent e)
     
     // creating conditions for button 4 click
     if (command.equals("4")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "4";
     }
     else{
@@ -287,7 +302,7 @@ public void actionPerformed(ActionEvent e)
      
      // creating conditions for button 5 click
     if (command.equals("5")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "5";
     }
     else{
@@ -297,7 +312,7 @@ public void actionPerformed(ActionEvent e)
      
      // creating conditions for button 6 click
     if (command.equals("6")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "6";
     }
     else{
@@ -307,7 +322,7 @@ public void actionPerformed(ActionEvent e)
     
     // creating conditions for button 7 click
     if (command.equals("7")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "7";
     }
     else{
@@ -317,7 +332,7 @@ public void actionPerformed(ActionEvent e)
     
     // creating conditions for button 8 click
     if (command.equals("8")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "8";
     }
     else{
@@ -327,7 +342,7 @@ public void actionPerformed(ActionEvent e)
      
      // creating conditions for button 9 click
     if (command.equals("9")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "9";
     }
     else{
@@ -337,7 +352,7 @@ public void actionPerformed(ActionEvent e)
      
      // creating conditions for button 0 click
     if (command.equals("0")) {
-        if(calcText == "0" || calcText == null){
+        if(calcText.equals("0")){
         calcText = "0";
     }
     else{
@@ -347,43 +362,44 @@ public void actionPerformed(ActionEvent e)
      
      // creating conditions for button point click
     if (command.equals(".")) {
-        calcText += point.getText();
+        if(!calcText.contains("."))
+        {
+            calcText += point.getText();
+        }
     }
     
     // creating conditions for button abs click
     if (command.equals("+/-")) {
-        calcText += "abs";
+        if(abs.equals("+"))
+        {
+            calcText += "-";
+        }
+        else{
+            calcText += "+";
+        }
     }
     
     // creating conditions for button + click
     if (command.equals("+")) {
-        firstNumber = Double.parseDouble(calcText);
-        operator = "+";
-        calcText = "0";
+        calcText += "+";
     }
     
     // creating conditions for button minus click
     if (command.equals("-")) {
         //calcText += minus.getText();
-        firstNumber = Double.parseDouble(calcText);
-        operator = "-";
-        calcText = "0";
+        calcText += "-";
     }
     
     // creating conditions for button multiply click
     if (command.equals("*")) {
         //calcText += times.getText();
-        firstNumber = Double.parseDouble(calcText);
-        operator = "*";
-        calcText = "0";
+        calcText += "*";
     }
     
     // creating conditions for button divide click
     if (command.equals("/")) {
         //calcText += divide.getText();
-        firstNumber = Double.parseDouble(calcText);
-        operator = "/";
-        calcText = "0";
+        calcText += "/";
     }
     
     // creating conditions for button factorial click
@@ -403,7 +419,7 @@ public void actionPerformed(ActionEvent e)
     
     // creating conditions for button off click
     if (command.equals("OFF")) {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.dispose();
     }
     
     // creating conditions for button clear click
@@ -412,31 +428,101 @@ public void actionPerformed(ActionEvent e)
         calcText = calcText.substring(0, len-1);
     }
     
+    // creating conditions for button clear click
+    if (command.equals("C")) {
+        calcText = "";
+    }
+    
     // creating conditions for button = click
     if (command.equals("=")) {
-        Double secondNumber = 0.00;
-        secondNumber = Double.parseDouble(calcText);
-        if(operator == "+"){
-            result= Double.toString(firstNumber + secondNumber);
-            calcText = result;
-        }
-        if(operator == "-"){
-            result= Double.toString(firstNumber - secondNumber);
-            calcText = result;
-        }
-        if(operator == "*"){
-            result= Double.toString(firstNumber * secondNumber);
-            calcText = result;
-        }
-        if(operator == "/"){
-            result= Double.toString(firstNumber / secondNumber);
-            calcText = result;
-        }
-        
+        calcText = findValueInBraces(calcText);
     }
     
     calText.setText(calcText);
     
+}
+
+public static String findValueInBraces(String finalStr) {
+
+    while (finalStr.contains("(") && finalStr.contains(")")) {
+        int fIndex = finalStr.indexOf("(");
+        int nIndex = finalStr.indexOf(")");
+        String subString = finalStr.substring(fIndex + 1, nIndex);
+        finalStr = finalStr.substring(0, fIndex)+ calculate(subString)+ finalStr.substring(nIndex + 1,finalStr.length());
+    }
+    return calculate(finalStr);
+
+}
+    
+public static String calculate(String finalString) {
+
+    while (finalString.contains("(") && finalString.contains(")")) {
+        findValueInBraces(finalString);
+    }
+    while (!isNum(finalString)) {
+        List<Integer> positions = getOperandPosition(finalString);
+        int pos = positions.get(0);
+        if (positions.size() >= 2 && positions.get(1) != null) {
+            int nxtPos = positions.get(1);
+            finalString = getValue(finalString.substring(0, nxtPos), pos)
+                    + finalString.substring(nxtPos, finalString.length());
+        } else {
+            finalString = getValue(
+                    finalString.substring(0, finalString.length()), pos);
+        }
+    }
+    return finalString;
+
+}
+
+public static boolean isNum(String str) {
+    if (str.contains("+") || str.contains("-") || str.contains("*")
+            || str.contains("/")) {
+        return false;
+    }
+    return true;
+}
+
+public static List<Integer> getOperandPosition(String str) {
+
+    List<Integer> integers = new ArrayList<Integer>();
+
+    if (str.contains("+")) {
+        integers.add(str.indexOf("+"));
+    }
+
+    if (str.contains("-")) {
+        integers.add(str.indexOf("-"));
+    }
+
+    if (str.contains("*")) {
+        integers.add(str.indexOf("*"));
+    }
+
+    if (str.contains("/")) {
+        integers.add(str.indexOf("/"));
+    }
+
+    Collections.sort(integers);
+    return integers;
+}
+
+public static String getValue(String str, int pos) {
+    double finalVal = 0;
+    double a = Double.parseDouble(str.substring(0, pos));
+    double b = Double.parseDouble(str.substring(pos + 1, str.length()));
+    char c = str.charAt(pos);
+
+    if (c == '*') {
+        finalVal = a * b;
+    } else if (c == '/') {
+        finalVal = a / b;
+    } else if (c == '+') {
+        finalVal = a + b;
+    } else if (c == '-') {
+        finalVal = a - b;
+    }
+    return String.valueOf(finalVal);
 }
 
     
